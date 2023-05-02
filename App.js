d9fcdca48054cb21bbc16e2ef7a6dec452e79c78
Camiscoding3 <108905466+Camiscoding3@users.noreply.github.com>
2023-05-02 16:22:41 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from "./screens/Home";
+import StartMapScreen from "./screens/StarMap";
+import DailyPicScreen from "./screens/DailyPic";
+import SpaceCraftScreen from "./screens/SpaceCraft";
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <View>
+      
+    </View>
+  )
+}
+
+export default App;
